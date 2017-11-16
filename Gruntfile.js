6f2c8a1e4e116077c5ce9c9543ce3a21f5c5f349
leachung41 <leachung41@gmail.com>
2017-11-16 00:32:55 -0500

    re scratch?

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,13 @@
+'use strict'
+
+var ngrok = require('ngrok');
+
 module.exports = function(grunt) {
 
+	// // Load grunt tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Grunt configuration
 	grunt.initConfig({
 		responsive_images: {
 			dev: {
@@ -8,25 +16,58 @@ module.exports = function(grunt) {
 					sizes: [{
 						name: 'five',
 						width: '5%',
-						quality: 20
-					},
-					{
-						name: 'seventy',
-						width: '100%',
 						quality: 30
+					},{
+						name: 'half',
+						width: '100%',
+						quality: 50
 					}]
 				},
-					files: [{
-						expand: true,
-						src: ['*.{gif,jpg,png,JPG}'],
-						cwd: 'img/opt_img',
-						dest: 'img/opt_img'
-					}]
+				files: [{
+					expand: true,
+					src: ['*.{gif,jpg,png,JPG}'],
+					cwd: 'img/opt_img',
+					dest: 'img/opt_img'
+				}]
+			}
+		},
+		pagespeed: {
+			options: {
+				nokey: true,
+				locale: "en_GB",
+				threshold: 40
+			},
+			local: {
+				options: {
+					strategy: "desktop"
 				}
 			},
-	  });
+			mobile: {
+				options: {
+					strategy: "mobile"
+				}
+			}
+		}
+	});
 
-	grunt.loadNpmTasks('grunt-responsive-images');
-	grunt.registerTask('default', ['responsive_images']);
+	// Register customer task for ngrok
+	grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
+		var done = this.async();
+		var port = 8000;
+
+		ngrok.connect(port, function(err, url) {
+			if (err !== null) {
+				grunt.fail.fatal(err);
+				return done();
+			}
+			grunt.config.set('pagespeed.options.url', url);
+			grunt.task.run('pagespeed');
+			done();
+		});
+	});
+
+	// Register default tasks
+	grunt.registerTask('default', ['responsive_images','psi-ngrok']);
+
 
 };
