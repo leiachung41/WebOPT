90103bf6ed752ac8b6414138094c374d2d405d21
leachung41 <leachung41@gmail.com>
2017-11-16 17:09:14 -0500

    changePizzaSizes

--- views/js/main.js
+++ views/js/main.js
@@ -421,7 +421,28 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
+  // Iterates through pizza elements on the page and changes their widths
+  function changePizzaSizes(size) {
+    switch(size) {
+      case "1":
+        newWidth = 25;
+        break;
+      case "2":
+        newWidth = 33.3;
+        break;
+      case "3":
+        newWidth = 50;
+        break;
+      default:
+        console.log("bug in sizeSwitcher");
+    }
+    var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+    for (var i=0; i<randomPizzas.length; i++) {
+        randomPizzas[i].style.width = newWidth + "%";
+    }
+  }
+
+/* // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
   function determineDx (elem, size) {
     var oldWidth = elem.offsetWidth;
     var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
@@ -454,8 +475,8 @@ var resizePizzas = function(size) {
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
-  } 
-
+  }
+*/
   changePizzaSizes(size);
 
   // User Timing API is awesome
